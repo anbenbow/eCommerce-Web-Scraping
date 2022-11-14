f7df4a6eb24f57507aed3a282a0eb9111586b4a1
Amber Benbow <anbenbow@gmail.comgit config --global user.email anbenbow@gmail.com>
2022-11-14 13:48:16 -0500

    imported pandas to sort

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 import csv
 import time
+import pandas as pandasforsortingcsv
 
 file=open("laptops.csv","w",newline="")
 writer=csv.writer(file)
@@ -13,29 +14,35 @@ browser_driver=Service("./chromedriver.exe")
 page=webdriver.Chrome(service=browser_driver)
 page.get("https://webscraper.io/test-sites/e-commerce/static/computers/laptops")
 
-ID=1
+
+ID=0
 while True:
-
-    
     laptop_thumbnails=page.find_elements(By.CLASS_NAME,"title")
- 
     for thumbnail in laptop_thumbnails:
-        time.sleep(3)
         thumbnail.click()
-        Name=page.find_element(By.XPATH,"//h4")
-        Price=page.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div[1]/h4[1]")
-        Specifications=page.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div[1]/p")
-        Number_Of_Reviews=page.find_element(By.XPATH,"//p")
-        page.back()
-        split_text=page.text.split(" ")
-        writer.writerow([ID,Name.text,Price.text,Specifications.text,Number_Of_Reviews.text])
+        time.sleep(3)
+        Name=page.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/div/div/div[2]/div[1]/h4[2]").text
+        Price=page.find_element(By.XPATH,"//h4").text
+        Specifications=page.find_element(By.CLASS_NAME,"description").text
+        Number_Of_Reviews=page.find_element(By.CLASS_NAME,"ratings").text
         ID += 1
-
+        writer.writerow([ID,Name,Price,Specifications,Number_Of_Reviews])
+        
+        page.back()
     try:
+        #element=page.find_element(By.XPATH,"/html/body/div[1]/div[3]/div/div[2]/ul/li[13]/a")
         element=page.find_element(By.PARTIAL_LINK_TEXT,"›")
         element.click()
     except NoSuchElementException:
         break    
+
+
+
+    file.close()
+    page.quit()
+
+csv_data=pandasforsortingcsv.read_csv("laptops.csv")
+csv_data.sort_values(["Price"],axis=0,ascending=[False],inplace=True)
 
 # ›
 
